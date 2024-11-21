1a53f371f706b390489d678ff5fc05c6a02b9bd6
zhudong <1541711389@qq.com>
2024-11-21 22:14:42 +0800

    创建layout和login组件并配置路由模块

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+// 路由配置
+import { createBrowserRouter } from "react-router-dom";
+
+// 注意！！！ 不要加{} 组件不需要加{}
+import  Layout  from "../pages/Layout";
+import  Login  from "../pages/Login";
+// 路由配置
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  }
+])
+
+export default router
