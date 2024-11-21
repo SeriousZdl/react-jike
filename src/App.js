a38092a698d73d2aa0a72b72e2ffafc94ceb13ad
zhudong <1541711389@qq.com>
2024-11-21 20:11:45 +0800

    安装ant组件-并测试是否安装成功

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import {Button} from 'antd';
+import React from 'react';
+
 function App (){
-  return <div>this is App</div>
+  return <div>this is App
+    <Button type="primary">click me</Button>
+  </div>
+  
 }
 export default App
