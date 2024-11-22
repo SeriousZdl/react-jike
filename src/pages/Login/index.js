409e61aff559a3b7039653b11e78311d00644474
zhudong <1541711389@qq.com>
2024-11-22 15:38:24 +0800

    通过使用Ant组件Form自带的固定属性onFinish获取表单的value

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,29 +3,33 @@ import { Card, Form, Input, Button } from 'antd'
 import logo from '@/assets/logo.png'
 
 const Login = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  }
   return (
     <div className="login">
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
-        <Form validateTrigger="onBlur">
+        <Form validateTrigger="onBlur" onFinish={onFinish}>
           <Form.Item
+
             name="mobile"
             // 多条检验逻辑 先校验第一条 第一条通过之后 再校验第二条
             rules={[
-              { 
-              required: true, message: '请输入手机号'
+              {
+                required: true, message: '请输入手机号'
               },
               {
                 pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确'
               }]}>
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item 
-          name="code"
-            rules={[{ 
-              required: true, message: '请输入验证码' 
-              }]}>
+          <Form.Item
+            name="code"
+            rules={[{
+              required: true, message: '请输入验证码'
+            }]}>
             <Input size="large" placeholder="请输入验证码" />
           </Form.Item>
           <Form.Item>
