6550016d6027aed9aa20e3974802dba6d7a1180c
zhudong <1541711389@qq.com>
2024-11-22 10:53:20 +0800

    配置@别名路径

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@
 import { createBrowserRouter } from "react-router-dom";
 
 // 注意！！！ 不要加{} 组件不需要加{}
-import  Layout  from "../pages/Layout";
-import  Login  from "../pages/Login";
+import  Layout  from "@/pages/Layout";
+import  Login  from "@/pages/Login";
 // 路由配置
 const router = createBrowserRouter([
   {
