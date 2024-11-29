89801f77504f457a8dd66107390916a08050913a
zhudong <1541711389@qq.com>
2024-11-29 10:31:52 +0800

    Layout-静态结构搭建和样式初始化-安装Normalize.css第三方初始化组件库

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import router from './router';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import 'normalize.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
